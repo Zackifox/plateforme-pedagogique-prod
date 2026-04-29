8cd4d9034d6981e6cf54e9eb956b02d946bf99b3
Zackifox <davidngomdjibaye@gmail.com>
2026-04-29 12:48:56 +0000

    Fix build configuration

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input --settings=config.settings.production
 python manage.py migrate --settings=config.settings.production
 python create_superuser.py
